a2d0cd662a8eaf32a25f0ff443e60d5d44a469f7
cylin1123 <l.chih.yu@gmail.com>
2018-09-05 09:15:17 +0800

    update

--- PermMissingElem.php
+++ PermMissingElem.php
@@ -1,5 +1,22 @@
 <?php
 
+$A =[2,3,1,5];
 
+echo solution($A);
+
+function solution($A) {
+    // write your code in PHP7.0
+    
+    $N = count($A);
+    
+    $Sum = ($N+1)*($N+2)/2;
+    
+    for($i=0 ; $i < $N; $i++){
+        
+        $Sum = $Sum - $A[$i];
+        
+    }
+    return $Sum;    
+}
 
 ?>
